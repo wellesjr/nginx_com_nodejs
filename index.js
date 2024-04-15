2eb85f199f7c7ddc4ea158099207e35d96d7645e
Welles Jr <welles-junior@hotmail.com>
2024-04-14 22:21:11 -0300

    Criação do index

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Welles Jr')`
+connection.query(sql)
+connection.end()
+
+app.get('/', (req, res) => {
+    res.send('<h1>Bem-vindo ao Node.js!</h1>');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
