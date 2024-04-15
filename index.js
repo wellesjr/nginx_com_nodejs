be318639b37925d91705defc2af419e4f075a725
Welles Jr <welles-junior@hotmail.com>
2024-04-14 23:04:07 -0300

    criado um arquivo .gitignore e ajuste no nginx.conf

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const config = {
-    host: 'db',
-    user: 'root',
-    password: 'root',
-    database: 'nodedb'
-};
+// const config = {
+//     host: 'db',
+//     user: 'root',
+//     password: 'root',
+//     database: 'nodedb'
+// };
 
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
+// const mysql = require('mysql')
+// const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Welles Jr')`
-connection.query(sql)
-connection.end()
+// const sql = `INSERT INTO people(name) values('Welles Jr')`
+// connection.query(sql)
+// connection.end()
 
 app.get('/', (req, res) => {
     res.send('<h1>Bem-vindo ao Node.js!</h1>');
